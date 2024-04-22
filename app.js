15e9046a49f5fe02f122ab60055883215ac63ca9
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-22 15:48:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Question 13: Your Own Array:
+var transports = ["70CC ", "125CC", "civic"];
+transports.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
+//Question 14: Guest List:
+var guests = ["Muhammad", "Anus", "Ahmed"];
+guests.forEach(function (guest) {
+    console.log("Dear ".concat(guest, ", would you like to join me for dinner?"));
+});
---
+++ app.ts
@@ -0,0 +1,15 @@
+// Question 13: Your Own Array:
+
+let transports: string[] = ["70CC ", "125CC", "civic"];
+
+transports.forEach(transport => {
+    console.log(`I would like to own a ${transport}.`);
+});
+
+//Question 14: Guest List:
+
+let guests: string[] = ["Muhammad", "Anus", "Ahmed"];
+
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
